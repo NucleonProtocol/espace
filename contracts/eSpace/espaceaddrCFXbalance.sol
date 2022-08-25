947fda820e40363c12cd1ef20abc533c79951e8a
article-i <artii@foxmail.com>
2022-08-25 19:40:11 +0800

    delete not used sol

--- contracts/eSpace/espaceaddrCFXbalance.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: BUSL-1.1
-
-pragma solidity ^0.8.0;
-
-contract espaceaddrCFXbalance  {
-    function balanceof(address _addr) public returns(uint256) {
-        return _addr.balance;
-    }
-}
